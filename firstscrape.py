8e48ed4be0da444bf53f9f9b2680cf72133cfb72
trhubwork <ted@brecklinecapital.com>
2016-05-03 20:21:48 -0600

    Add terrain update and heading for information.

--- firstscrape.py
+++ firstscrape.py
@@ -5,11 +5,23 @@ bsObj = BeautifulSoup(html.read())
 skiCondi = bsObj.findAll("span", {"class" : "current_status open"})
 skiOpCl = bsObj.findAll("span", {"class" : "open_close"})
 
+greenSki = bsObj.findAll("span", {"class" : "ovv_t t1"})
+blueSki = bsObj.findAll("span", {"class" : "ovv_t t2"})
+blackSki = bsObj.findAll("span", {"class" : "ovv_t t3"})
+blackdiaSki = bsObj.findAll("span", {"class" : "ovv_t t4"})
+
+for green in greenSki:
+    print("Today's Ski Conditions \nGreen Slopes: " + green.get_text())
+for blue in blueSki:
+    print("Blue Slopes: " + blue.get_text())
+for black in blackSki:
+    print("Black Slopes: " + black.get_text())
+for blackdia in blackdiaSki:
+    print("Black Diamonds: " + blackdia.get_text())
 for skiO in skiOpCl:
     print(skiO.get_text())
-
 for ski in skiCondi:
-    print(ski.get_text())
+    print("Open Days of the week: "+ ski.get_text())
 
 
 
