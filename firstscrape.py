7e306c74be9a96078e9453d0fe3333cb0eef7707
trhubwork <ted@brecklinecapital.com>
2016-05-03 20:04:21 -0600

    Add a ski venue to scrape open season and current days of the week open.

--- firstscrape.py
+++ firstscrape.py
@@ -1,6 +1,15 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = urlopen("https://en.wikipedia.org/wiki/Main_Page")
+html = urlopen("http://www.onthesnow.com/vermont/jay-peak/ski-resort.html")
 bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+skiCondi = bsObj.findAll("span", {"class" : "current_status open"})
+skiOpCl = bsObj.findAll("span", {"class" : "open_close"})
+
+for skiO in skiOpCl:
+    print(skiO.get_text())
+
+for ski in skiCondi:
+    print(ski.get_text())
+
+
 
